5fd39a9363cfc6c583426b65c3d0ef916ac26768
colonca <c_colon10@hotmail.com>
2021-09-05 02:11:27 -0500

    github action deploy

--- server_deploy.sh
+++ server_deploy.sh
@@ -9,21 +9,4 @@ cd /var/wwww
 git fetch origin master
 git reset --hard origin/master
 
-docker exec -ti pebi-app bash
-
-# Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
-
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
-
-    # Migrate database
-    php artisan migrate --force
-
-    # Clear cache
-    php artisan optimize
-
-# Exit maintenance mode
-php artisan up
-
 echo "Application deployed!"
