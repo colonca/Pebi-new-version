24653f04332d283521af722e27fe6608dd2aea5e
ccolon <c_colon10@hotmail.com>
2022-08-27 12:52:50 -0500

    fix: ajuste del reporte por tipos de riesgos

--- app/Http/Livewire/Reportes/Riesgos.php
+++ app/Http/Livewire/Reportes/Riesgos.php
@@ -23,9 +23,10 @@ class Riesgos extends Component
     public $header = ['PROGRAMA'];
     public $riesgos = [];
 
-    public function mount(){
+    public function mount()
+    {
         $this->riesgos = Riesgo::all(['descripcion']);
-        foreach ($this->riesgos as $riesgo){
+        foreach ($this->riesgos as $riesgo) {
             $this->header[] = $riesgo->descripcion;
         }
     }
@@ -48,11 +49,11 @@ class Riesgos extends Component
     public function render()
     {
 
-        $programas = Programas::orderBy('nombre','ASC')->get()->mapWithKeys(function ($item) {
+        $programas = Programas::orderBy('nombre', 'ASC')->get()->mapWithKeys(function ($item) {
             return [$item['id'] => $item];
         });
 
-        $periodos = PeriodosAcademicos::orderBy('fecha_inicio','DESC')->get()->take(6)->mapWithKeys(function ($item) {
+        $periodos = PeriodosAcademicos::orderBy('fecha_inicio', 'DESC')->get()->take(6)->mapWithKeys(function ($item) {
             return [$item['id'] => $item];
         });
         /*
@@ -65,31 +66,27 @@ class Riesgos extends Component
         $query =  DB::table('estudiantes as e')
             ->select([
                 'p.nombre as programa',
-                'r.descripcion as riesgo',
+                'h.riesgo as riesgo',
                 DB::raw('count(*) as cantidad')
-            ])->join('programas as p','p.id','=','e.programa_id')
-            ->join('historico as h','e.id','=','h.estudiante_id')
-            ->join('riesgos as r','r.id','=','h.riesgo_id')
-            ->groupBy('p.nombre','r.descripcion')
+            ])->join('programas as p', 'p.id', '=', 'e.programa_id')
+            ->join('historico as h', 'e.id', '=', 'h.estudiante_id')
+            ->groupBy('p.nombre', 'h.riesgo')
             ->when($this->filtros['periodo'], function ($query) {
-                return $query->where('h.periodo_id',$this->filtros['periodo']);
+                return $query->where('h.periodo_id', $this->filtros['periodo']);
             })
             ->when($this->filtros['programa'], function ($query) {
-                return $query->where('p.id',$this->filtros['programa']);
+                return $query->where('p.id', $this->filtros['programa']);
             })
             ->get();
-
         $reporte = [];
-        foreach ($query as $item){
+        foreach ($query as $item) {
             $reporte[$item->programa][$item->riesgo] = $item->cantidad;
         }
 
-        return view('livewire.reportes.riesgos',[
+        return view('livewire.reportes.riesgos', [
             'programas' => $programas,
             'periodos' => $periodos,
             'reporte' => $reporte
         ]);
     }
 }
-
-
